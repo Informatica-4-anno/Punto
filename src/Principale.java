bb6ddb968aea809ba221cf5eb8e4eb9c5215e40d
Roberto Spotti <roberto.spotti@gmail.com>
2020-12-04 13:32:06 +0100

    Passaggio parametri, shallow e deep copy

--- src/Principale.java
+++ src/Principale.java
@@ -2,28 +2,55 @@
 public class Principale {
 
 	public static void main(String[] args) {
-
-		Punto p; // Allocazione della variabile che conterrà l'indirizzo dell'oggetto
 		
-		p = new Punto(3,8);  // Creazione dell'oggetto Punto (con richiamo del costruttore)
-		                     // Assegna l'indirizzo dell'oggetto alla variabile p di tipo punto
+		// Reference      Oggetto
+		Punto p       =  new Punto(4,5);
 		
-		
-		Punto p1 = new Punto(2.2,6);
-		
-		System.out.println(p);
-		System.out.println(p1);
-		
-		System.out.println(p.distance(p1));
-		System.out.println(p1.distance(p));
-		System.out.println(p.distance(p));
+		Punto p1 = p; // Shallow --> copia il reference dell'oggetto
+		              //    Due reference che puntano allo stesso oggetto!!!
 		
 		
+		p1.move(2, -2);
 		
-		// Distanza tra p e p1???
+		System.out.println("Ecco p1="+p1);
+		System.out.println("Ecco p="+p);
 		
+		Punto p2=new Punto(p1.getX(),p1.getY());  // Deep copy
+		Punto p3=new Punto(p2  /* Parametro attuale */ );  //  usando il copy constructor 
 		
-
+		double a=6;
+		double b=5;
+		p2.move(a, b); /* parametri attuali passati per valore */ 
+		/*
+		 * Con il passaggio dei parametri per valore anche se nel metodo move
+		 * un parametro (in questo caso a) viene modificato, all'uscita dal 
+		 * metodo il parametro non viene modificato.
+		 * Come si vede dalla seguente System.out.println	
+		 */
+		System.out.println("a="+a); 
+		
+		p3.move(2, 1); /* Con il passaggio per valore posso quindi passare anche
+		               ** valori costanti
+		               */
+		System.out.println("Ecco p2="+p2);
+		System.out.println("Ecco p3="+p3);
+		
+		/*
+		 * Il metodo align invece allinea i punti, cambiando l'ordinata
+		 * del punto passato come parametro, quindi modifica il valore 
+		 * dell'oggetto parametro, questo è possibile grazie al passaggio
+		 * per referenza che vuol dire che io passo il reference (ovvero il 
+		 * riferimento all'oggetto) non l'oggetto stesso (come la shallow copy)
+		 * quindi posso modificare l'oggetto perchè l'area di memoria che 
+		 * lo contiene non viene duplicata.
+		 * Non posso cambiare invece il valore del reference, ovvero se all'interno
+		 * del metodo creassi un altro oggetto punto, il cambiamento non si 
+		 * riperquoterebbe all'esterno del metodo stesso. 
+		 */
+		p3.allign(p2); /* Passaggio parametri per referenza */
+		
+		System.out.println("Ecco p2="+p2);
+		System.out.println("Ecco p3="+p3);	
 	}
 
 }
--- src/Punto.java
+++ src/Punto.java
@@ -4,10 +4,30 @@ public class Punto {
 	private double y;
 
 	public Punto(double x, double y) {
+		System.out.println("Eseguo il costruttore!!");
 		this.x=x;
 		this.y=y;
+		System.out.println("Fine costruttore");
 	}
-
+	
+	public Punto(Punto p /* Parametro formale */) {  // Copy constructor
+		System.out.println("Eseguo il copy constructor");
+		this.x=p.getX();
+		this.y=p.getY();
+	}
+	
+	public void move(double deltax, double deltay) {
+		x=x+deltax;   // x+=deltax;
+		y=y+deltay;   // y+=deltay;
+		deltax=99;
+	}
+	
+	public void allign(Punto p) {
+		p.y=this.y;  // copia del valore
+		p=new Punto(99,99); // questa istruzione invece non avrà effetto 
+		                    // fuori da questo metodo!!! (non posso cambiare il valore di p)
+	}
+	
 	public double getX() {
 		return x;
 	}
